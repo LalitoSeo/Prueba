820bdc65e90fc4b969dcf58827330c8eb853779e
LalitoSeo <ed.isco@hotmail.com>
2020-03-11 00:52:31 -0600

    Inicial

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar app color="primary" dense dark>
+            <v-toolbar-title>Titulo</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn text to="/">Inicio</v-btn>
+                <v-btn text to="/home">Menu1</v-btn>
+                <v-btn text>Menu2</v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
